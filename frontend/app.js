c86fe0fa98f2bf8b212f8bbd6d7794b8ba89aeff
Official-Vicks <chidieberevictory218@gmail.com>
2026-04-08 13:04:58 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 let lastLogs = [];
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://smart-automation-hub.onrender.com";
 
 // 🔒 Locks to prevent overlapping requests
 let isFetchingLogs = false;
